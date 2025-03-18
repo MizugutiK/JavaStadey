35a6a19d068d5a26c7b8e9c90a34ad4e3fdc3032
DESKTOP-CS56K2P\MZGT <m0a20268c5@edu.teu.ac.jp>
2025-03-18 15:24:39 +0900

    課題1　

--- Javatest/src/App.java
+++ Javatest/src/App.java
@@ -1,5 +1,8 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!11111");
+        System.out.println("Java練習");
     }
+
 }
+
