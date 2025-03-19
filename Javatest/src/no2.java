a3a4da1f3dcb3ec0472ec9c3700fbaa9c26155dd
DESKTOP-CS56K2P\MZGT <m0a20268c5@edu.teu.ac.jp>
2025-03-19 16:19:23 +0900

    課題２

---
+++ Javatest/src/no2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class no2 {
+    public static void main(String[] args) {
+        try {
+            // FileWriterクラスのオブジェクトを生成する
+            FileWriter file = new FileWriter("/Users/MZGT/Documents/JavaStadey/Javatest/testfolda/java.txt");
+            // PrintWriterクラスのオブジェクトを生成する
+            PrintWriter pw = new PrintWriter(new BufferedWriter(file));
+
+            // ファイルに書き込む
+            pw.println("Hellow World");
+            
+            System.out.println("java.txtに書き込まれました");
+
+            // ファイルを閉じる
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
